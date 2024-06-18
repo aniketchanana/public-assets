857ac146a6725c3899d502c71b69ce151c9e41f0
aniket <aniket.chanana@gmail.com>
2024-06-18 17:51:30 +0530

    nits

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const getAllFiles = function (dirPath, arrayOfFiles) {
 // Function to create README file
 const createReadme = function (files) {
   const readmeContent = `# Directory Listing\n\n## Files\n\n${files
-    .map((file) => `- ${f}`)
+    .map((file) => `- ${file}`)
     .join('\n')}`;
 
   fs.writeFileSync('README.md', readmeContent, function (err) {
